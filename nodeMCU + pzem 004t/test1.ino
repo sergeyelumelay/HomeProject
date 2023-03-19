0aeee1c8547be9ba0c7abe3d62e2d34bcf56d855
sergeyelumelay <sergeyelumelay9@gmail.com>
2023-03-19 21:31:11 +0800

    Source code for arduino IDE(nodeMCU)

---
+++ nodeMCU + pzem 004t/test1.ino
@@ -0,0 +1,72 @@
+// nodeMCU library
+#include <ESP8266WiFi.h>
+// pzem-004t library
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+// firebase library
+
+PZEM004Tv30 pzem(D3, D4); // Software Serial pin 8 (RX) & 9 (TX)
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float voltage = pzem.voltage();
+  if (voltage != NAN) {
+    Serial.print("Voltage: ");
+    Serial.print(voltage);
+    Serial.println("V");
+  } else {
+    Serial.println("Error reading voltage");
+  }
+
+  float current = pzem.current();
+  if (current != NAN) {
+    Serial.print("Current: ");
+    Serial.print(current);
+    Serial.println("A");
+  } else {
+    Serial.println("Error reading current");
+  }
+
+  float power = pzem.power();
+  if (current != NAN) {
+    Serial.print("Power: ");
+    Serial.print(power);
+    Serial.println("W");
+  } else {
+    Serial.println("Error reading power");
+  }
+
+  float energy = pzem.energy();
+  if (current != NAN) {
+    Serial.print("Energy: ");
+    Serial.print(energy, 3);
+    Serial.println("kWh");
+
+  } else {
+    Serial.println("Error reading energy");
+  }
+
+  float frequency = pzem.frequency();
+  if (current != NAN) {
+    Serial.print("Frequency: ");
+    Serial.print(frequency, 1);
+    Serial.println("Hz");
+  } else {
+    Serial.println("Error reading frequency");
+  }
+
+  float pf = pzem.pf();
+  if (current != NAN) {
+    Serial.print("PF: ");
+    Serial.println(pf);
+  } else {
+    Serial.println("Error reading power factor");
+  }
+
+  Serial.println();
+  delay(2000);
+}
